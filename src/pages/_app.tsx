d8f43c185436f40a9f0fe8036b36ff1cd5efa63e
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-01-11 17:29:28 -0300

    updated next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
-import { AppProps } from 'next/app';
-import { Header } from '../components/Header';
-import { Provider as NextAuthProvider } from 'next-auth/client';
-
-import '../styles/global.scss';
-
-function MyApp({ Component, pageProps }) {
+import { AppProps } from "next/app"
+import { Header } from "../components/Header"
+import { SessionProvider } from "next-auth/react"
+import '../styles/global.scss'
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </NextAuthProvider>
-  );
+    </SessionProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
